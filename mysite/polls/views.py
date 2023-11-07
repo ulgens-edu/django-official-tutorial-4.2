48f8c4d7d5b41c3c76853e7d8b9dd82a8a918063
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2023-11-07 06:30:50 +0300

    Raise 404 for non-existent question IDs

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -13,7 +13,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
 
 
