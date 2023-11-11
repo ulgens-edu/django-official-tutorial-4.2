4368b8083cddc33e5f78f75c3868ad4b0cb2ded9
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2023-11-11 21:43:10 +0300

    Update folder names to my taste

--- src/core/settings.py
+++ src/core/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "mysite.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "mysite.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
